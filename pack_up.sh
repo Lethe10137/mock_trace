979d3cabeb196ca3d692390dc8c88c846c927311
Lethe Lee 160 <lichenghaongyd@qq.com>
2024-09-17 06:21:23 +0000

    feat: choose some networks

--- pack_up.sh
+++ pack_up.sh
@@ -1,13 +1,10 @@
-rm mock_toml/*
-rm mock_trace/*
-
 cargo run --release
 
-rm mock_trace.zip
-rm mock_toml.zip
+# rm mock_trace.zip
+# rm mock_toml.zip
 
 zip -r mock_trace mock_trace
 zip -r mock_toml mock_toml
 
-rsync -avz mock_trace.zip lich@101.6.30.160:~/upload/
-rsync -avz mock_toml.zip lich@101.6.30.160:~/upload/
+# rsync -avz mock_trace.zip lich@101.6.30.160:~/upload/
+# rsync -avz mock_toml.zip lich@101.6.30.160:~/upload/
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,64 @@ use netem_trace::{Bandwidth, Duration};
 use netem_trace::{Mahimahi, MahimahiExt};
 
 use polars::prelude::*;
+use std::collections::HashSet;
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
 
 use rayon::prelude::*;
 
+
+use std::fs;
+use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+// 封装的函数，处理指定目录的重命名和创建
+fn backup_and_create_dir(dir_name: &str) -> std::io::Result<()> {
+    // 获取当前时间戳
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_secs();
+
+    // 定义备份目录的名称
+    let backup_dir_name = format!("{}_backup_{}", dir_name, timestamp);
+
+    // 检查并重命名目录
+    if Path::new(dir_name).exists() {
+        fs::rename(dir_name, &backup_dir_name)?;
+        println!("Renamed '{}' to '{}'", dir_name, backup_dir_name);
+    }
+
+    // 创建新的目录
+    fs::create_dir(dir_name)?;
+    println!("Created new directory '{}'", dir_name);
+
+    Ok(())
+}
+
+
 fn main() -> Result<(), Box<dyn Error>> {
+
+    backup_and_create_dir("mock_trace")?;
+    backup_and_create_dir("mock_toml")?;
+
+
+    let chosen = std::fs::File::open("choose.txt").map(
+        |file: File| io::BufReader::new(file)
+        .lines()
+        .into_iter()
+        .map(|line| line.unwrap())
+        .collect::<HashSet<_>>()
+    );
+
+
+    let filter = |str: &String| {
+        chosen.is_err() || chosen.as_ref().is_ok_and(|chosen| 
+            chosen.contains(str.as_str().split_at(24).0))
+    };
+
+
     // 读取 .parquet 文件
     let mut file = std::fs::File::open("mock.txt").unwrap();
 
@@ -20,7 +71,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         .lines()
         .into_iter()
         .map(|line| line.unwrap())
+        .filter(filter)
         .collect();
+
+
+    
 
     lines.par_iter().for_each(|line| {
         let split_line: Vec<String> = line
@@ -47,7 +102,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn write(file: &str, delay: f64, loss: f64, tput: Vec<f64>) {
-    let std_dev_options = [1.0, 0.5, 0.25, 0.125, 0.0625, 0.03125];
+    let std_dev_options = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
     let queue_length_options = [100, 200];
 
     let mut traces = vec![];
