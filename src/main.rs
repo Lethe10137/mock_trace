ccf7a88ebe45ff09565b86f3672138cd0f99a07d
Lethe Lee 160 <lichenghaongyd@qq.com>
2024-09-22 08:00:34 +0000

    vary tput

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use netem_trace::model::{BwTraceConfig, RepeatedBwPatternConfig};
-use netem_trace::model::{NormalizedBwConfig, StaticBwConfig};
+use netem_trace::model::NormalizedBwConfig;
 use netem_trace::{Bandwidth, Duration};
-use netem_trace::{Mahimahi, MahimahiExt};
+use netem_trace::MahimahiExt;
 
-use polars::prelude::*;
+
 use std::collections::HashSet;
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
 
 use rayon::prelude::*;
-
 
 use std::fs;
 use std::path::Path;
@@ -40,30 +39,27 @@ fn backup_and_create_dir(dir_name: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-
 fn main() -> Result<(), Box<dyn Error>> {
-
     backup_and_create_dir("mock_trace")?;
     backup_and_create_dir("mock_toml")?;
 
-
-    let chosen = std::fs::File::open("choose.txt").map(
-        |file: File| io::BufReader::new(file)
-        .lines()
-        .into_iter()
-        .map(|line| line.unwrap())
-        .collect::<HashSet<_>>()
-    );
-
+    let chosen = std::fs::File::open("choose.txt").map(|file: File| {
+        io::BufReader::new(file)
+            .lines()
+            .into_iter()
+            .map(|line| line.unwrap())
+            .collect::<HashSet<_>>()
+    });
 
     let filter = |str: &String| {
-        chosen.is_err() || chosen.as_ref().is_ok_and(|chosen| 
-            chosen.contains(str.as_str().split_at(24).0))
+        chosen.is_err()
+            || chosen
+                .as_ref()
+                .is_ok_and(|chosen| chosen.contains(str.as_str().split_at(24).0))
     };
 
-
     // 读取 .parquet 文件
-    let mut file = std::fs::File::open("mock.txt").unwrap();
+    let file = std::fs::File::open("mock.txt").unwrap();
 
     let reader = io::BufReader::new(file);
 
@@ -73,9 +69,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(|line| line.unwrap())
         .filter(filter)
         .collect();
-
-
-    
 
     lines.par_iter().for_each(|line| {
         let split_line: Vec<String> = line
@@ -102,50 +95,61 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn write(file: &str, delay: f64, loss: f64, tput: Vec<f64>) {
-    let std_dev_options = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
+    let std_dev_options = [
+        0.03125, 0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
+    ];
+    let tput_times = [0.5, 0.75, 1.25, 1.5, 2.0];
     let queue_length_options = [100, 200];
+
+    // Too many arguments!
+    // https://docs.rs/netem-trace/latest/netem_trace/model/bw/struct.SawtoothBw.html
 
     let mut traces = vec![];
 
-    for std_dev in std_dev_options {
-        let tput = tput
-            .iter()
-            .cloned()
-            .map(|tput_seocnd| {
-                // Box::new(
-                //     StaticBwConfig::new()
-                //         .bw(Bandwidth::from_kbps((tput_seocnd * 1000.0) as u64))
-                //         .duration(Duration::from_millis(1000)),
-                // ) as Box<dyn BwTraceConfig>
-                Box::new(
-                    NormalizedBwConfig::new()
-                        .mean(Bandwidth::from_kbps((tput_seocnd * 1000.0) as u64))
-                        .std_dev(Bandwidth::from_kbps(
-                            (tput_seocnd * 1000.0 * std_dev) as u64,
-                        ))
-                        .duration(Duration::from_secs(1)),
-                ) as Box<dyn BwTraceConfig>
-            })
-            .collect();
+    for tput_time in tput_times {
+        for std_dev in std_dev_options {
+            let tput = tput
+                .iter()
+                .cloned()
+                .map(|tput_seocnd| {
+                    // Box::new(
+                    //     StaticBwConfig::new()
+                    //         .bw(Bandwidth::from_kbps((tput_seocnd * 1000.0) as u64))
+                    //         .duration(Duration::from_millis(1000)),
+                    // ) as Box<dyn BwTraceConfig>
+                    Box::new(
+                        NormalizedBwConfig::new()
+                            .mean(Bandwidth::from_kbps(
+                                (tput_seocnd * 1000.0 * tput_time) as u64,
+                            ))
+                            .std_dev(Bandwidth::from_kbps(
+                                (tput_seocnd * 1000.0 * std_dev * tput_time) as u64,
+                            ))
+                            .duration(Duration::from_secs(1)),
+                    ) as Box<dyn BwTraceConfig>
+                })
+                .collect();
 
-        let mut c = Box::new(RepeatedBwPatternConfig::new().pattern(tput).count(2)).into_model();
+            let mut c =
+                Box::new(RepeatedBwPatternConfig::new().pattern(tput).count(2)).into_model();
 
-        c.mahimahi_to_file(
-            &Duration::from_secs(120),
-            format!("mock_trace/{}_{}.trace", file, std_dev),
-        );
+            c.mahimahi_to_file(
+                &Duration::from_secs(120),
+                format!("mock_trace/{}_{}_{}.trace", file, std_dev, tput_time),
+            );
 
-        for queue_len in queue_length_options {
-            traces.push(format!(
-                r#"
-        {{ type = "BwReplay", trace = "./mock_trace/{}_{}.trace", queue = "DropTail", queue_config = {{ packet_limit = {} }} }},"#, file, std_dev, queue_len
-            ))
+            for queue_len in queue_length_options {
+                traces.push(format!(
+                    r#"
+            {{ type = "BwReplay", trace = "./mock_trace/{}_{}_{}.trace", queue = "DropTail", queue_config = {{ packet_limit = {} }} }},"#, file, std_dev, tput_time, queue_len
+                ))
+            }
         }
     }
 
     // 构造 TOML 配置字符串
     let toml_content = format!(
-        r#"labels = ["NYC_mock0912"]
+        r#"labels = ["NYC_mock0922_varytput"]
 [network_set.uplink]
 delay = [{{ type = "Delay", delay = "{}ms" }}]
 bandwidth = [{{ type = "Bw", bandwidth = "100Mbps", queue = "Infinite" }}]
